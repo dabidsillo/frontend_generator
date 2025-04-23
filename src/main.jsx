17daf3512e1772053c809af05447612112a94d93
DarioSuarezL <dsuarezlazarte@gmail.com>
2025-04-22 23:06:39 -0400

    tailwindcss

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <h1 className='bg-red-500'>Hola mundo</h1>
   </StrictMode>,
 )
